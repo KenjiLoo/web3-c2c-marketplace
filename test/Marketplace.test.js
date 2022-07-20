ed15eda19257215cd53cf341d3056985431a0bd6
KenjiLoo <kenjilooxy@gmail.com>
2022-07-20 19:17:01 +0800

    added new unit test

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -16,14 +16,20 @@ contract('Marketplace', (accounts) => {
 
     //using chai built in truffle
     describe('deployment', async() => {
+
+        //checks if smart contract address is not null
         it('deployed successfully :D', async () => {
             const address = await marketplace.address
-
-            //checks if smart contract address is not null
             assert.notEqual(address, 0x0)
             assert.notEqual(address, '')
             assert.notEqual(address, null)
             assert.notEqual(address, undefined)
+        })
+
+        //checks if smart contract has a name (not null, and set to smart contract constructor public var "name" in this case)
+        it('name checked :D', async () =>{
+            const name = await marketplace.name()
+            assert.equal(name, "Web3 C2C Marketplace")
         })
     })
 
