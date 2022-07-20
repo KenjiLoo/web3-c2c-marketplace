11880971cbc25e41d9e4cf68c47174e88fca3e0b
KenjiLoo <kenjilooxy@gmail.com>
2022-07-20 21:56:11 +0800

    create product test wrtten

--- src/contracts/Markeplace.sol
+++ src/contracts/Markeplace.sol
@@ -5,8 +5,35 @@ pragma solidity >=0.4.21 <0.9.0;
 //run "truffle migrate" after ensuring connection with Ganache and migration script for this smart contract is set up 
 contract Marketplace{ 
     string public name;
+
+    //keep track of product amount
+    uint public productCount = 0;
+
+    //create mapping for products (hash table/ key-value)
+    //this state var puts the products to the blockchain
+    mapping(uint => Product) public products; //products is the name of the mapping
+
+    //create struct for products
+    struct Product{
+        uint id;
+        string name;
+        uint price; 
+        address owner;
+        bool purchased;
+    }
     
     constructor() public{
         name = "Web3 C2C Marketplace";
+    }
+
+    function createProduct() public{
+        //make sure parameters are correct
+        
+        //create product
+
+        //increment productCount
+        productCount ++;
+
+        //trigger an event
     }
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,11 +1,11 @@
 //test file for marketplace smart contract
 
+const { assert } = require("chai")
+
 //import smart contract
 const Marketplace = artifacts.require('./Marketplace.sol')
 
 contract('Marketplace', (accounts) => {
-    //tests inside here 
-
     let marketplace
 
     // happens in truffle console
@@ -15,10 +15,11 @@ contract('Marketplace', (accounts) => {
     })
 
     //using chai built in truffle
-    describe('deployment', async() => {
+    //deployment related tests
+    describe('Deployment', async() => {
 
         //checks if smart contract address is not null
-        it('deployed successfully :D', async () => {
+        it('Deployed successfully :D', async () => {
             const address = await marketplace.address
             assert.notEqual(address, 0x0)
             assert.notEqual(address, '')
@@ -27,10 +28,29 @@ contract('Marketplace', (accounts) => {
         })
 
         //checks if smart contract has a name (not null, and set to smart contract constructor public var "name" in this case)
-        it('name checked :D', async () =>{
+        it('Name checked :D', async () =>{
             const name = await marketplace.name()
             assert.equal(name, "Web3 C2C Marketplace")
         })
+
+    })
+
+    //product related tests
+    describe('Products', async() => {
+        let result, productCount
+
+        before(async () => {
+            result = await marketplace.createProduct()
+            productCount = await marketplace.productCount()
+        })
+
+        //checks if smart contract has a created product
+        it('Created products :D', async () =>{
+            
+            //check product count
+            assert.equal(productCount, 1)
+        })
+
     })
 
 })
